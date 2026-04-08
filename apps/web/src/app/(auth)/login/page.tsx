d899ae98d05fc740dde2e804bc5d3378b433d859
Douglas <douglasmsrodrigues@hotmail.com>
2026-04-08 14:26:42 -0300

    Aplicando o cursos pointer nos elementos

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -68,7 +68,7 @@ export default function LoginPage({ searchParams }: LoginPageProps) {
       <form action={loginGoogle}>
         <button
           type="submit"
-          className="w-full py-3 px-4 rounded-lg text-sm font-medium border border-gray-200 bg-white hover:bg-gray-50 transition-all flex items-center justify-center gap-3"
+          className="cursor-pointer w-full py-3 px-4 rounded-lg text-sm font-medium border border-gray-200 bg-white hover:bg-gray-50 transition-all flex items-center justify-center gap-3"
         >
           <svg className="w-5 h-5" viewBox="0 0 24 24">
             <path
--- apps/web/src/components/ui/Button.tsx
+++ apps/web/src/components/ui/Button.tsx
@@ -12,14 +12,15 @@ export function Button({
   icon,
 }: ButtonProps) {
   const styles = {
-    primary: "bg-[#1B5E20] text-white hover:bg-[#145214] shadow-lg hover:shadow-xl",
+    primary:
+      "cursor-pointer  bg-[#1B5E20] text-white hover:bg-[#145214] shadow-lg hover:shadow-xl",
     secondary: "bg-gray-100 text-gray-700 hover:bg-gray-200",
   }
 
   return (
     <button
       type={type}
-      className={`w-full py-3 px-4 rounded-lg text-sm font-medium transition-all flex items-center justify-center gap-2 ${styles[variant]}`}
+      className={`cursor-pointer w-full py-3 px-4 rounded-lg text-sm font-medium transition-all flex items-center justify-center gap-2 ${styles[variant]}`}
     >
       {icon}
       {label}
