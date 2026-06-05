13d940d04cc2e9ea5431209676352d6af49fe3ba
Douglas <douglasmsrodrigues@hotmail.com>
2026-06-05 10:44:27 -0300

    implementando o clerk

--- apps/web/src/app/(auth)/layout.tsx
+++ apps/web/src/app/(auth)/layout.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link"
 import { ArrowLeft } from "lucide-react"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  Show,
+} from "@clerk/nextjs"
 
 export default function AuthLayout({
   children,
@@ -27,6 +34,24 @@ export default function AuthLayout({
             <p className="text-sm text-[#2E7D32] mt-1">
               Gestão simples para MEI
             </p>
+            <ClerkProvider>
+              <ClerkProvider>
+                <header className="flex justify-end items-center p-4 gap-4 h-16">
+                  <Show when="signed-out">
+                    <SignInButton />
+                    <SignUpButton>
+                      <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                        Sign Up
+                      </button>
+                    </SignUpButton>
+                  </Show>
+                  <Show when="signed-in">
+                    <UserButton />
+                  </Show>
+                </header>
+                {children}
+              </ClerkProvider>
+            </ClerkProvider>
           </div>
           {children}
         </div>
